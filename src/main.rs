58aee50de262024721b922ccc7f365d4feb1a3d4
Anish Sinha <anishsinha0128@gmail.com>
2024-09-25 02:33:27 -0400

    Update workflow

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use std::collections::HashMap;
+
+use anyhow::Result;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let resp = reqwest::get("https://httpbin.org/ip")
+        .await?
+        .json::<HashMap<String, String>>()
+        .await?;
+    println!("{resp:#?}");
+    Ok(())
 }
