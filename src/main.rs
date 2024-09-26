9aefca119d31130b59ac59b87bc3a1aa7abe7fd5
Anish Sinha <anishsinha0128@gmail.com>
2024-09-25 23:20:53 -0400

    Add nats support

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 mod errors;
+use async_nats::Message;
 use errors::AppError;
+use futures::StreamExt;
 use rand::distributions::Alphanumeric;
 use rand::Rng;
+use tokio::{select, task};
 
 use std::collections::HashMap;
+use std::env;
+use std::sync::Arc;
 
 use anyhow::Result;
 use axum::http::StatusCode;
@@ -23,8 +28,49 @@ struct User {
     name: String,
 }
 
+async fn process_events(
+    client: async_nats::Client,
+    topic: &str,
+    msgs: Arc<tokio::sync::Mutex<Vec<Message>>>,
+) -> Result<()> {
+    let mut sub = client.subscribe(topic.to_string()).await?;
+    loop {
+        select! {
+            res = sub.next() => {
+                if let Some(msg) = res {
+                    println!("Received message: {:?}", msg);
+                    msgs.lock().await.push(msg);
+                }
+            }
+        }
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
+    let msgs = Arc::new(tokio::sync::Mutex::new(Vec::<Message>::new()));
+
+    let (nats_token, nats_host) = (env::var("NATS_TOKEN"), env::var("NATS_HOST"));
+    let nats_url = match (nats_token, nats_host) {
+        (Ok(token), Ok(host)) => format!("{}@{}:4222", token, host),
+        (_, _) => "nats://localhost:4222".to_string(),
+    };
+
+    println!("Connecting to NATS server at: {}", nats_url);
+
+    let client = async_nats::connect(&nats_url).await?;
+
+    let msgs_copy = msgs.clone();
+
+    task::spawn(async move {
+        match process_events(client.clone(), "asdf.asdf", msgs.clone()).await {
+            Ok(_) => {}
+            Err(e) => {
+                eprintln!("Error: {:?}", e);
+            }
+        }
+    });
+
     let app = Router::new()
         .route(
             "/",
@@ -74,7 +120,20 @@ async fn main() -> Result<()> {
                 })
             }),
         )
-        .route("/ip", routing::get(get_ip));
+        .route("/ip", routing::get(get_ip))
+        .route(
+            "/events",
+            routing::get(|| async move {
+                let x = msgs_copy.clone().lock().await.clone();
+
+                (
+                    StatusCode::OK,
+                    Json(Data {
+                        msg: format!("[NEW]: received {} messages", x.len()),
+                    }),
+                )
+            }),
+        );
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8181").await?;
     axum::serve(listener, app).await?;
